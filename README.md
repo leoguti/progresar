622021a5506355137cd1c9e7684f8dda00ddff7c
Leonardo Gutierrez <leogiga@gmail.com>
2017-08-31 22:07:56 -0500

    Se agregaron los procedimientos de medicion y se cargaron al sketch

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Tambien se debe agregar la libreria de thinger, esta se encuentra en.
 https://github.com/thinger-io/Arduino-Library/archive/master.zip
 
 Esta libreria permite que el fona se comunique directamente como iot al sistema de Thinger.io
+
+El cableado con el arduino uno se puede encontrar en: 
+
+https://learn.adafruit.com/adafruit-fona-mini-gsm-gprs-cellular-phone-module/wiring
--- progresar/progresar.ino
+++ progresar/progresar.ino
@@ -15,7 +15,7 @@
 // Emulate Serial1 on pins 10/11 if HW is not present (use interrupt pin in RX for better performance)
 #ifndef HAVE_HWSERIAL1
 #include "SoftwareSerial.h"
-SoftwareSerial Serial1(3, 2); // RX, TX
+SoftwareSerial Serial1(9, 2); // RX, TX
 #endif
 
 #define USERNAME "Ubilogica"
@@ -29,6 +29,30 @@ SoftwareSerial Serial1(3, 2); // RX, TX
 
 // set your cad pin (optional)
 #define CARD_PIN ""
+
+
+//Variables para conductividad 
+
+long pulseCount = 0;  //a pulse counter variable
+unsigned long pulseTime,lastTime, duration, totalDuration;
+int interruptPin = 1; //corresponds to D2
+int samplingPeriod=3; // the number of seconds to measure 555 oscillations
+
+//--------------------------
+
+//thermistor stuff -----------------------
+#define THERMISTORPIN A0         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000    
 
 ThingerTinyGSM thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL, Serial1);
 
@@ -49,12 +73,83 @@ void setup() {
   pinMode(13, OUTPUT);
   thing["led"] << digitalPin(13);
 
+  thing["sensores"] >> [](pson& out){
+        out["temperature"] = temperature();
+        out["conductivity"] = conductivity();
+  };
+
   // resource output example (i.e. reading a sensor value)
-  thing["millis"] >> outputValue(millis());
+  //thing["millis"] >> outputValue(millis());
+
+  //thing["conductivity"] >> outputValue(conductivity());
+
+  //thing["temperature"] >> outputValue(temperature());
 
   // more details at http://docs.thinger.io/arduino/
 }
 
 void loop() {
   thing.handle();
+}
+
+
+
+
+
+float temperature(){
+    uint8_t i;
+    float average;
+    int samples[NUMSAMPLES];
+    // take N samples in a row, with a slight delay
+    for (i=0; i< NUMSAMPLES; i++) {
+      samples[i] = analogRead(THERMISTORPIN);
+      delay(10);
+    }
+    // average all the samples out
+    average = 0;
+    for (i=0; i< NUMSAMPLES; i++) {
+      average += samples[i];
+    }
+    average /= NUMSAMPLES;
+    
+    // convert the value to resistance
+    average = 1023 / average - 1;
+    average = SERIESRESISTOR / average;
+    
+    float steinhart;
+    steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = log(steinhart);                  // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart;                 // Invert
+    steinhart -= 273.15;                         // convert to C
+    return steinhart;    
+}
+
+
+
+float conductivity(){
+  attachInterrupt(interruptPin,onPulse,RISING); //attach an interrupt counter to interrupt pin 1 (digital pin #3) -- the only other possible pin on the 328p is interrupt pin #0 (digital pin #2)
+  pulseTime=micros(); // start the stopwatch
+  delay(samplingPeriod*1000); //give ourselves samplingPeriod seconds to make this measurement, during which the "onPulse" function will count up all the pulses, and sum the total time they took as 'totalDuration' 
+  detachInterrupt(interruptPin); //we've finished sampling, so detach the interrupt function -- don't count any more pulses
+  float freqHertz;
+  if (pulseCount>0) { //use this logic in case something went wrong
+    double durationS=(totalDuration/double(pulseCount))/1000000.; //the total duration, in seconds, per pulse (note that totalDuration was in microseconds)
+    freqHertz=1./durationS;
+  }
+  else {
+    freqHertz=0.;
+  }
+  return freqHertz;
+}
+
+
+void onPulse()  //ver si esta en COnd
+{
+  pulseCount++;
+  lastTime = pulseTime;
+  pulseTime = micros();
+  duration=pulseTime-lastTime;
+  totalDuration+=duration;
 }
